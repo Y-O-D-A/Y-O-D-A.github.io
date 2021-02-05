faf89323eb7ceab7602e263b6042a59c9f395d9c
Shane Crawford <scford805@outlook.com>
2021-02-05 09:41:38 -0800

    added messages

--- Javascript.js
+++ Javascript.js
@@ -6,6 +6,17 @@ function add() {
 
     document.querySelector('h1').innerHTML = x
 
+    if (x === 100){
+        alert('Good Job! You got to 100')
+    }
+
+    if (x === 1000){
+        alert('AMAZING! YOU GOT TO 1000!')
+    }
+
+    if (x === 10000) {
+        alert('You did a very good job of hacking this')
+    }
 }
 
 function subtract() {
@@ -13,6 +24,18 @@ function subtract() {
     x--
 
     document.querySelector('h1').innerHTML = x
+
+    if (x === 100){
+        alert('Good Job! You got to 100')
+    }
+
+    if (x === 1000){
+        alert('AMAZING! YOU GOT TO 1000!')
+    }
+
+    if (x === 10000) {
+        alert('You did a very good job of hacking this')
+    }
 
 }
 
